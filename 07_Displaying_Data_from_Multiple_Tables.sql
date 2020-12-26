ac5626aae0195351e0b25cf3e05256ac745d3926
Tim Yang <tyang@wustl.edu>
2020-12-26 14:56:03 +0900

    12/25/2020 11:56 PM

--- 07_Displaying_Data_from_Multiple_Tables.sql
+++ 07_Displaying_Data_from_Multiple_Tables.sql
@@ -1,17 +1,17 @@
 
 /*
 Displaying Data from Multiple Tables
-- Using SELF-JOINS
-- Using various types of JOINS
-- Using non equiJoins
-- Using OUTER JOINS
 - Understanding and Using Cartesian Products
+- Using various types of Joins
+- Using non Equijoins
+- Using Outer Joins
+- Using Self-Joins
 */
 
 
 /*
 Cartesian Products:
-    - Retreiving data from multiple tables without using JOINS
+    - Retreiving data from multiple tables without using Joins
 
 A Cartesian Product is formed when:
     - A join condition is omitted
@@ -22,15 +22,15 @@ To avoid a Cartesian product, always include a valid JOIN condition in a WHERE c
 */
 
 SELECT employee_id, first_name, department_id       -- department_id is a foreign key to the Departments table
-FROM Employees;
+FROM   Employees;
 
 SELECT department_id, department_name
-FROM Departments;
+FROM   Departments;
 
 -- Cartesian Product
-SELECT Employees.employee_id, Employees.first_name, Employees.last_name,
-       Departments.department_id, Departments.department_name
-FROM Employees, Departments                         -- 107 rows (Employees) X 27 rows (Departments) = 2889 rows
+SELECT   Employees.employee_id, Employees.first_name, Employees.last_name,
+         Departments.department_id, Departments.department_name
+FROM     Employees, Departments                         -- 107 rows (Employees) X 27 rows (Departments) = 2889 rows
 ORDER BY employee_id;                               -- each row in Employees are joined with each row in Departments
 
 
@@ -39,7 +39,7 @@ Types of Joins:
 
 1. Oracle Proprietary Joins (8i and prior):
     - Equijoin
-    - Noneequijoin
+    - Nonequijoin
     - Outer Join
     - Self Join
     
@@ -53,33 +53,33 @@ Types of Joins:
 
 
 --1 Equijoin (Simple Joins / Inner Joins)
-SELECT Employees.employee_id, Employees.first_name, Employees.department_id,
-       Departments.department_name          -- department_id exists in both tables, so should not remove the table
-FROM Employees, Departments
-WHERE Employees.department_id = Departments.department_id
+SELECT   Employees.employee_id, Employees.first_name, Employees.department_id,
+         Departments.department_name          -- department_id exists in both tables, so should not remove the table
+FROM     Employees, Departments
+WHERE    Employees.department_id = Departments.department_id
 ORDER BY employee_id;
 
 -- using additional conditions
-SELECT Employees.employee_id, Employees.first_name, Employees.department_id,
-       Departments.department_id, Departments.department_name
-FROM Employees, Departments
-WHERE Employees.department_id = Departments.department_id
-      AND Employees.department_id > 40
+SELECT   Employees.employee_id, Employees.first_name, Employees.department_id,
+         Departments.department_id, Departments.department_name
+FROM     Employees, Departments
+WHERE    Employees.department_id = Departments.department_id
+         AND Employees.department_id > 40
 ORDER BY employee_id;
 -- using table Alias
-SELECT Emp.employee_id, Emp.first_name, Emp.department_id,
-       Dept.department_name
-FROM   Employees Emp, Departments Dept
-WHERE  Emp.department_id = Dept.department_id
+SELECT   Emp.employee_id, Emp.first_name, Emp.department_id,
+         Dept.department_name
+FROM     Employees Emp, Departments Dept
+WHERE    Emp.department_id = Dept.department_id
 ORDER BY employee_id;
 
 -- joining more than 2 tables
-SELECT Emp.employee_id, Emp.first_name, Emp.department_id,
-       Dept.department_name, Dept.location_id,
-       Loc.city
-FROM Employees Emp, Departments Dept, Locations Loc
-WHERE Emp.department_id = Dept.department_id 
-      AND Dept.location_id = Loc.location_id
+SELECT   Emp.employee_id, Emp.first_name, Emp.department_id,
+         Dept.department_name, Dept.location_id,
+         Loc.city
+FROM     Employees Emp, Departments Dept, Locations Loc
+WHERE    Emp.department_id = Dept.department_id 
+         AND Dept.location_id = Loc.location_id
 ORDER BY employee_id;
 
 
@@ -96,15 +96,15 @@ highest_sal NUMBER
 INSERT INTO Job_grades (grade_level, lowest_sal, highest_sal)
 VALUES ('A', 1000, 2999);
 INSERT INTO Job_grades (grade_level, lowest_sal, highest_sal)
-VALUES ('A', 3000, 5999);
+VALUES ('B', 3000, 5999);
 INSERT INTO Job_grades (grade_level, lowest_sal, highest_sal)
-VALUES ('A', 6000, 9999);
+VALUES ('C', 6000, 9999);
 INSERT INTO Job_grades (grade_level, lowest_sal, highest_sal)
-VALUES ('A', 10000, 14999);
+VALUES ('D', 10000, 14999);
 INSERT INTO Job_grades (grade_level, lowest_sal, highest_sal)
-VALUES ('A', 15000, 24999);
+VALUES ('E', 15000, 24999);
 INSERT INTO Job_grades (grade_level, lowest_sal, highest_sal)
-VALUES ('A', 25000, 40000);
+VALUES ('F', 25000, 40000);
 
 COMMIT;
 
@@ -115,3 +115,100 @@ SELECT Emp.employee_id, Emp.first_name, Emp.salary,
        Grades.grade_level
 FROM   Employees Emp, Job_grades Grades
 WHERE  Emp.salary BETWEEN Grades.lowest_sal AND Grades.highest_sal;
+
+-- the above is the same as
+SELECT Emp.employee_id, Emp.first_name, Emp.salary, 
+       Grades.grade_level
+FROM   Employees Emp, Job_grades Grades
+WHERE  Emp.salary >= Grades.lowest_sal
+       AND Emp.salary <= Grades.highest_sal;
+       
+
+--3 Outer Join (+), Casel
+-- all data on the "opposite" side of (+) will appear
+
+-- this is an Equijoin example 
+SELECT   Employees.employee_id, Employees.first_name, Employees.department_id,
+         Departments.department_name
+FROM     Employees, Departments
+WHERE    Employees.department_id = Departments.department_id
+ORDER BY employee_id;
+
+-- here is the Outer Join 
+-- Employees is the main table
+SELECT   Employees.employee_id, Employees.first_name, Employees.department_id,
+         Departments.department_name
+FROM     Employees, Departments
+WHERE    Employees.department_id = Departments.department_id(+)
+ORDER BY employee_id;
+
+-- Departments is the main table
+SELECT   Employees.employee_id, Employees.first_name, Employees.department_id,
+         Departments.department_name
+FROM     Employees, Departments
+WHERE    Employees.department_id(+) = Departments.department_id
+ORDER BY employee_id;
+
+-- Exercise
+/*
+Retrieve all the Employees (employee_id, first_name, department_id) who's salary > 2500
+and display their Department department_name, location_id, 
+and Locations city and country_id
+and Countries country_name.
+All employees should appear even if they have no department
+*/
+
+-- determine how many records should be returned
+SELECT   count(1)
+FROM     Employees
+WHERE    salary > 2500;
+
+--
+SELECT   Emp.employee_id,     Dept.department_name,   Loc. city,      Cont.country_name,      
+         Emp.first_name,      Dept.location_id,    
+         Emp.department_id
+         
+FROM     Employees Emp,       Departments Dept,       Locations Loc,  Countries Cont
+
+WHERE    Emp.department_id = Dept.department_id(+)
+         AND Dept.location_id = Loc.location_id(+)
+         AND Loc.country_id = Cont.country_id(+)
+         AND salary > 2500
+
+ORDER BY employee_id;
+
+
+--4 Self Joins
+SELECT employee_id, first_name, manager_id
+FROM   Employees;
+
+-- I want to display the manager name, so it is a Self Join
+SELECT Worker.employee_id, Worker.first_name, Worker.manager_id,
+       Manager.first_name
+FROM   Employees Worker, Employees Manager
+WHERE  Worker.manager_id = Manager.employee_id;
+
+SELECT Worker.employee_id, Worker.first_name, Worker.manager_id,
+       Manager.first_name
+FROM   Employees Worker, Employees Manager
+WHERE  Worker.manager_id = Manager.employee_id(+);
+
+
+--5 SQL: 1999 Syntax
+/*
+- Cross Joins
+- Natural Joins
+- Using Clause
+- Full or Two Sided Outer Joins
+- Arbitrary Join Conditions for Outer Joins
+*/
+
+-- Cross Join
+-- same as Cartesian Product
+SELECT   last_name, department_name
+FROM     Employees, Departments;
+
+-- the only difference is replacing "," with CROSS JOIN
+SELECT   last_name, department_name
+FROM     Employees CROSS JOIN Departments;
+
