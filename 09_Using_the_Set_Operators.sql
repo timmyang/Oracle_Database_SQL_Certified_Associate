f3e852e1f7e565c4ceb4c63351478516d53f975c
Tim Yang <tyang@wustl.edu>
2020-12-31 22:02:57 -0600

    12/31/2020 10:02 PM

---
+++ 09_Using_the_Set_Operators.sql
@@ -0,0 +1,20 @@
+
+/*
+Using the Set Operators
+- Matching the SELECT statements
+- Using the ORDER BY clause in set operations
+- Using The INTERSECT operator
+- Using The MINUS operator
+- Using The UNION and UNION ALL operators
+*/
+
+/*
+Set Operators combine the results of two or more component queries into one result.
+Queries containing Set Operators are called "compound queries".
+
+UNION: returns rows from both queries after eliminating duplicates
+UNION ALL: retruns rows from both queries, including all duplicates
+INTERSECT: returns rows that are common to both queries
+MINUS: returns rows in the first query that are not present in the second query
+*/
+
